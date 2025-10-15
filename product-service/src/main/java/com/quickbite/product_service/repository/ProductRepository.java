ac5153ff4ea5330f79409a09245a1eec6b6eeae3
Israel Cadorin <cadorinisrael@gmail.com>
2025-10-15 01:21:16 -0300

    feat: add new methods to productRepository

--- product-service/src/main/java/com/quickbite/product_service/repository/ProductRepository.java
+++ product-service/src/main/java/com/quickbite/product_service/repository/ProductRepository.java
@@ -4,6 +4,8 @@ import com.quickbite.product_service.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -12,7 +14,21 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByCategoryIdAndIsAvailableTrue(Long categoryId);
 
+    List<Product> findByIsAvailableTrue();
+
     List<Product> findByRestaurantIdAndCategoryIdAndIsAvailableTrue(Long restaurantId, Long categoryId);
+
+    List<Product> findByNameContainingIgnoreCaseAndIsAvailableTrue(String name);
+
+    List<Product> findByRestaurantIdAndPriceBetweenAndIsAvailableTrue(
+        Long restaurantId,
+        BigDecimal minPrice,
+        BigDecimal maxPrice
+    );
+
+    List<Product> findByIsFeaturedTrueAndIsAvailableTrue();
+
+    Long countByRestaurantIdAndIsAvailableTrue(Long restaurantId);
 
     @Query("""
     SELECT p
@@ -22,21 +38,4 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
       AND LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))
     """)
     List<Product> searchAvailableProductsByName(@Param("name") String name);
-
-    @Query("""
-    SELECT p
-    FROM Product p
-    WHERE p.isAvailable = true
-      AND p.restaurant.id = :restaurantId
-      AND p.price BETWEEN :minPrice AND :maxPrice
-    """)
-    List<Product> findAvailableProductsByRestaurantAndPriceRange(
-            @Param("restaurantId") Long restaurantId,
-            @Param("minPrice") Double minPrice,
-            @Param("maxPrice") Double maxPrice
-    );
-
-    List<Product> findByIsFeaturedTrueAndIsAvailableTrue();
-
-    Long countByRestaurantIdAndIsAvailableTrue(Long restaurantId);
 }
