092a5f19e3825c81692d5b07e93c5958ef75e6c8
icadorin <cadorinisrael@gmail.com>
2025-11-13 23:57:17 -0300

    feat: enhance dto register request for user response

--- auth-service/src/main/java/com/quickbite/auth_service/dto/RegisterRequest.java
+++ auth-service/src/main/java/com/quickbite/auth_service/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.quickbite.auth_service.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,13 +15,15 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest {
 
     @NotBlank(message = "Nome completo é obrigatório")
+    @Size(min = 2, message = "Nome completo deve ter pelo menos 2 caracteres")
     private String fullName;
 
     @NotBlank(message = "Email é obrigatório")
-    @Email(message = "Email deve ser válido")
+    @Email(message = "Formato de emil inválido")
     private String email;
 
     @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter pelo menos 6 caracteres")
     private String password;
 
     private String phone;
