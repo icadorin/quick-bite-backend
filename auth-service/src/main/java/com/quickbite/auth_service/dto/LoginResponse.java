ad74d942ad7f090f8c5b5902ec7220a29a599d4b
icadorin <cadorinisrael@gmail.com>
2026-01-04 23:50:42 -0300

    add all-args and no-agrs contrtuctors to LoginResponse for testing

--- auth-service/src/main/java/com/quickbite/auth_service/dto/LoginResponse.java
+++ auth-service/src/main/java/com/quickbite/auth_service/dto/LoginResponse.java
@@ -1,10 +1,14 @@
 package com.quickbite.auth_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
     private String accessToken;
     private String refreshToken;
