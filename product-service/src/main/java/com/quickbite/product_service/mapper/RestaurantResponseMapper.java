9d997c1154fd0adac4827df2c351dd7b215e27ff
icadorin <cadorinisrael@gmail.com>
2026-01-13 00:02:47 -0300

    refactor(mapper): remove redundant mapstruct mappings

--- product-service/src/main/java/com/quickbite/product_service/mapper/RestaurantResponseMapper.java
+++ product-service/src/main/java/com/quickbite/product_service/mapper/RestaurantResponseMapper.java
@@ -3,15 +3,12 @@ package com.quickbite.product_service.mapper;
 import com.quickbite.product_service.dto.RestaurantResponse;
 import com.quickbite.product_service.entity.Restaurant;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RestaurantResponseMapper {
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "ownerId", target = "ownerId")
     RestaurantResponse toResponse(Restaurant restaurant);
 
     List<RestaurantResponse> toResponseList(List<Restaurant> restaurants);
