06b6275e5bc3f4334e699c8e2b7034d14e5784b3
icadorin <cadorinisrael@gmail.com>
2026-01-26 21:39:52 -0300

    fix: align ApiPaths constants with controller mappings

--- auth-service/src/main/java/com/quickbite/auth_service/constants/ApiPaths.java
+++ auth-service/src/main/java/com/quickbite/auth_service/constants/ApiPaths.java
@@ -5,9 +5,10 @@ public final class ApiPaths {
     private ApiPaths() {}
 
     public static final String API = "/api";
-    public static final String AUTH = "/auth";
+
+    public static final String AUTH = API + "/auth";
 
     public static final String REGISTER = "/register";
     public static final String LOGIN = "/login";
-    public static final String REFRESH_TOKEN = "/refresh_token";
+    public static final String REFRESH_TOKEN = "/refresh-token";
 }
