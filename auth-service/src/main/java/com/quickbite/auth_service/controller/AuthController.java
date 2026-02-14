c86b85ff8277def0965c943bcc355fe7a7d6d813
icadorin <cadorinisrael@gmail.com>
2026-02-14 03:12:48 -0300

    feat(auth): add logout endpoint

--- auth-service/src/main/java/com/quickbite/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/quickbite/auth_service/controller/AuthController.java
@@ -1,14 +1,12 @@
 package com.quickbite.auth_service.controller;
 
 import com.quickbite.auth_service.constants.ApiPaths;
-import com.quickbite.auth_service.dto.LoginResponse;
-import com.quickbite.auth_service.dto.RefreshTokenRequest;
-import com.quickbite.auth_service.dto.RegisterRequest;
-import com.quickbite.auth_service.dto.LoginRequest;
+import com.quickbite.auth_service.dto.*;
 import com.quickbite.auth_service.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +19,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(ApiPaths.REGISTER)
+    @PreAuthorize("permitAll()")
     @ResponseStatus(HttpStatus.CREATED)
     public LoginResponse register(
         @Valid @RequestBody RegisterRequest request
@@ -29,6 +28,7 @@ public class AuthController {
     }
 
    @PostMapping(ApiPaths.LOGIN)
+   @PreAuthorize("permitAll()")
     public LoginResponse login(
         @Valid @RequestBody LoginRequest request
     ) {
@@ -36,9 +36,19 @@ public class AuthController {
     }
 
     @PostMapping(ApiPaths.REFRESH_TOKEN)
+    @PreAuthorize("permitAll()")
     public LoginResponse refreshToken(
         @Valid @RequestBody RefreshTokenRequest request
     ) {
         return authService.refreshToken(request.getRefreshToken());
+    }
+
+    @PostMapping(ApiPaths.LOGOUT)
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(
+        @Valid @RequestBody LogoutRequest request
+    ) {
+        authService.logout(request.refreshToken());
     }
 }
