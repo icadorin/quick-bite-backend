985a52f5a3435fc1b85272eae00c20da0fe40f75
icadorin <cadorinisrael@gmail.com>
2026-02-14 03:19:19 -0300

    refactor(user): extend jpaSepcification for dynamic filtering

--- auth-service/src/main/java/com/quickbite/auth_service/repository/UserRepository.java
+++ auth-service/src/main/java/com/quickbite/auth_service/repository/UserRepository.java
@@ -1,25 +1,22 @@
 package com.quickbite.auth_service.repository;
 
 import com.quickbite.auth_service.entity.User;
-import com.quickbite.core.security.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends
+    JpaRepository<User, Long>,
+    JpaSpecificationExecutor<User> {
 
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
-
-    List<User> findByStatus(User.UserStatus status);
-
-    List<User> findByRole(UserRole role);
 
     @Query("""
         SELECT COUNT(u) > 0
