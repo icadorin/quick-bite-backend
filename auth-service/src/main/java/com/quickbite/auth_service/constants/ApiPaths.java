845f1875ca32c63977546d9e2db6a22d8c29f086
icadorin <cadorinisrael@gmail.com>
2026-02-14 03:12:21 -0300

    refactor(api): add logout and users paths to ApiPaths constants

--- auth-service/src/main/java/com/quickbite/auth_service/constants/ApiPaths.java
+++ auth-service/src/main/java/com/quickbite/auth_service/constants/ApiPaths.java
@@ -1,14 +1,16 @@
 package com.quickbite.auth_service.constants;
 
-public final class ApiPaths {
+public final class  ApiPaths {
 
     private ApiPaths() {}
 
     public static final String API = "/api";
 
     public static final String AUTH = API + "/auth";
-
     public static final String REGISTER = "/register";
     public static final String LOGIN = "/login";
     public static final String REFRESH_TOKEN = "/refresh-token";
+    public static final String LOGOUT = "/logout";
+
+    public static final String USERS =  API + "/users";
 }
