a2c27b5652c90c72d5c78288a5e9cde5791f6f6a
icadorin <cadorinisrael@gmail.com>
2026-03-03 23:49:28 -0300

    refactor(order-service): restructure order module and improve architecture

--- order-service/src/main/java/com/quickbite/order_service/dto/OrderRequest.java
+++ order-service/src/main/java/com/quickbite/order_service/dto/OrderRequest.java
@@ -1,5 +1,6 @@
 package com.quickbite.order_service.dtos;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -27,5 +28,6 @@ public class OrderRequest {
     private String paymentMethod;
 
     @NotEmpty
+    @Valid
     private List<OrderItemRequest> items;
 }
