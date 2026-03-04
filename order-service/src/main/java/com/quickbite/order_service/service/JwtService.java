07761eb3db2d5126fb3887da43cf29c7e50debe9
icadorin <cadorinisrael@gmail.com>
2026-03-04 00:10:28 -0300

    feat(security): add jwtSerivce for token validation and claim extraction

---
+++ order-service/src/main/java/com/quickbite/order_service/service/JwtService.java
@@ -0,0 +1,50 @@
+package com.quickbite.order_service.service;
+
+import com.quickbite.core.exception.InvalidTokenException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+
+    public JwtService(
+        @Value("${security.jwt.secret}") String secret
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token);
+        } catch (ExpiredJwtException ex) {
+            throw new InvalidTokenException("Token expired");
+        } catch (JwtException ex) {
+            throw new InvalidTokenException("Invalid token");
+        }
+    }
+
+    public String getRoleFromToken(String token) {
+        return getClaims(token).get("role", String.class);
+    }
+
+    public Long getUserIdFromToken(String token) {
+        return getClaims(token).get("userId", Long.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+}
