0645615af4cd5e537143f5c2346400a6e486d856
icadorin <cadorinisrael@gmail.com>
2026-03-04 00:00:49 -0300

    refactor(order-repository): enable specification support and simplify queries

--- order-service/src/main/java/com/quickbite/order_service/repositories/OrderRepository.java
+++ order-service/src/main/java/com/quickbite/order_service/repositories/OrderRepository.java
@@ -2,33 +2,22 @@ package com.quickbite.order_service.repositories;
 
 import com.quickbite.order_service.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends
+    JpaRepository<Order, Long>,
+    JpaSpecificationExecutor<Order> {
 
-    List<Order> findByUserId(Long userId);
-
-    List<Order> findByRestaurantId(Long restaurantId);
-
-    List<Order> findByStatus(Order.OrderStatus status);
-
-    List<Order> findByUserIdAndStatus(Long userId, Order.OrderStatus status);
-
-    List<Order> findByRestaurantIdAndStatus(Long restaurantId, Order.OrderStatus status);
-
-    @Query("SELECT o FROM Order o WHERE o.userId = :userId AND o.createdAt BETWEEN :startDate AND :endDate")
-    List<Order> findByUserIdAndCreateAtBetween(
-        @Param("userId") Long userId,
-        @Param("startDate") LocalDateTime startDate,
-        @Param("endDate") LocalDateTime endDate
-    );
-
-    @Query("SELECT COUNT(o) FROM Order o WHERE o.restaurantId = :restaurantId AND o.status = :status")
+    @Query("""
+        SELECT COUNT(o)
+        FROM Order o
+        WHERE o.restaurantId = :restaurantId
+            AND o.status = :status
+    """)
     Long countByRestaurantAndStatus(
         @Param("restaurantId") Long restaurantId,
         @Param("status") Order.OrderStatus status
