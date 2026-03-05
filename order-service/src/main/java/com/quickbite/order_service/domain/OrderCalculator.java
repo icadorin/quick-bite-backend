4b72303ad25535b8698a5bd8cf5fd9bcf1ca313e
icadorin <cadorinisrael@gmail.com>
2026-03-04 22:23:04 -0300

    refactor(order): convert OrderCalculator into spring component

--- order-service/src/main/java/com/quickbite/order_service/domain/OrderCalculator.java
+++ order-service/src/main/java/com/quickbite/order_service/domain/OrderCalculator.java
@@ -3,11 +3,13 @@ package com.quickbite.order_service.domain;
 import com.quickbite.core.exception.BusinessRuleViolationException;
 import com.quickbite.order_service.dto.OrderItemRequest;
 import com.quickbite.order_service.dto.ProductResponse;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class OrderCalculator {
 
     public BigDecimal calculateTotal(
