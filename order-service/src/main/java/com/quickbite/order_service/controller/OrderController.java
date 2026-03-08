c740718f584e6e528ae000ebe30c02092c96693c
icadorin <cadorinisrael@gmail.com>
2026-03-08 19:34:36 -0300

    refactor(controller): improve enpoints, add pagination and security

--- order-service/src/main/java/com/quickbite/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/quickbite/order_service/controller/OrderController.java
@@ -3,12 +3,18 @@ package com.quickbite.order_service.controller;
 import com.quickbite.order_service.constants.ApiPaths;
 import com.quickbite.order_service.dto.OrderRequest;
 import com.quickbite.order_service.dto.OrderResponse;
+import com.quickbite.order_service.dto.OrderStatusResponse;
 import com.quickbite.order_service.dto.OrderStatusUpdateRequest;
+import com.quickbite.order_service.dto.filter.OrderFilter;
 import com.quickbite.order_service.security.JwtUser;
 import com.quickbite.order_service.service.OrderService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +36,7 @@ public class OrderController {
         return orderService.getUserOrders(user.id());
     }
 
-    @GetMapping(ApiPaths.BY_ID)
+    @GetMapping(ApiPaths.ORDER_ID)
     public OrderResponse getOrderById(
         @PathVariable("id") @Positive Long id,
         @AuthenticationPrincipal JwtUser user
@@ -42,11 +48,27 @@ public class OrderController {
         );
     }
 
-    @GetMapping(ApiPaths.RESTAURANT + ApiPaths.BY_RESTAURANT_ID)
-    public List<OrderResponse> getRestaurantOrders(
-        @PathVariable("restaurantId") @Positive Long restaurantId
+    @GetMapping(ApiPaths.BY_RESTAURANT)
+    public Page<OrderResponse> getRestaurantOrders(
+        @PathVariable("restaurantId") @Positive Long restaurantId,
+        @PageableDefault(size = 20, sort = "createdAt") Pageable pageable
     ){
-        return orderService.getRestaurantOrders(restaurantId);
+        return orderService.getRestaurantOrders(restaurantId, pageable);
+    }
+
+    @GetMapping("/search")
+    public Page<OrderResponse> searchOrders(
+        OrderFilter filter,
+        Pageable pageable
+    ) {
+        return orderService.searchOrders(filter, pageable);
+    }
+
+    @GetMapping(ApiPaths.BY_RESTAURANT + "/stats")
+    public OrderStatusResponse getRestaurantStats(
+        @PathVariable("restaurantId") Long restaurantId
+    ) {
+        return orderService.getRestaurantStats(restaurantId);
     }
 
     @PostMapping
@@ -57,7 +79,8 @@ public class OrderController {
         return orderService.createOrder(request, user.id());
     }
 
-    @PatchMapping(ApiPaths.BY_ID + ApiPaths.STATUS)
+    @PatchMapping(ApiPaths.ORDER_ID + ApiPaths.STATUS)
+    @PreAuthorize("hasRole('RESTAURANT')")
     public OrderResponse updateOrderStatus(
         @PathVariable("id") @Positive Long id,
         @Valid @RequestBody OrderStatusUpdateRequest request,
@@ -66,7 +89,7 @@ public class OrderController {
         return orderService.updateOrderStatus(id, request, user.id(), user.role());
     }
 
-    @PostMapping(ApiPaths.BY_ID + ApiPaths.CANCEL)
+    @PostMapping(ApiPaths.ORDER_ID + ApiPaths.CANCEL)
     public OrderResponse cancelOrder(
         @PathVariable("id") @Positive Long id,
         @AuthenticationPrincipal JwtUser user
