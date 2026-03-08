f52d840102b4f80f6d7fe9efe86a1b61eb26c0b1
icadorin <cadorinisrael@gmail.com>
2026-03-08 19:31:51 -0300

    refactor(apiPaths): rename API constants

--- order-service/src/main/java/com/quickbite/order_service/constants/ApiPaths.java
+++ order-service/src/main/java/com/quickbite/order_service/constants/ApiPaths.java
@@ -4,14 +4,16 @@ public final class ApiPaths {
 
     private ApiPaths() {}
 
-    public static final String API = "/api/v1";
+    public static final String API_V1 = "/api/v1";
 
-    public static final String ORDERS = API + "/orders";
+    public static final String ORDERS = API_V1 + "/orders";
 
-    public static final String BY_ID = "/{id}";
+    public static final String ORDER_ID = "/{id}";
+    public static final String RESTAURANT_ID = "/{restaurantId}";
+
     public static final String STATUS = "/status";
     public static final String CANCEL = "/cancel";
 
     public static final String RESTAURANT = "/restaurant";
-    public static final String BY_RESTAURANT_ID = "/{restaurantId}";
+    public static final String BY_RESTAURANT = RESTAURANT + RESTAURANT_ID;
 }
