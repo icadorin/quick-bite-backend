877b9465c33207677acd43cdc8b6a65ac7280b70
icadorin <cadorinisrael@gmail.com>
2026-03-08 20:03:38 -0300

    feat(dto): add getter, setter and constructors

--- order-service/src/main/java/com/quickbite/order_service/dto/DeliveryAddress.java
+++ order-service/src/main/java/com/quickbite/order_service/dto/DeliveryAddress.java
@@ -1,5 +1,12 @@
 package com.quickbite.order_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeliveryAddress {
 
     private String street;
