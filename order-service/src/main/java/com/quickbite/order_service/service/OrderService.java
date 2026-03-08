09c8ba1c1e4b34d8ee1e4ab0caac0f19292967ae
icadorin <cadorinisrael@gmail.com>
2026-03-08 19:45:06 -0300

    feat(orderService): add searchOrder and restaurantStats methods

--- order-service/src/main/java/com/quickbite/order_service/service/OrderService.java
+++ order-service/src/main/java/com/quickbite/order_service/service/OrderService.java
@@ -2,9 +2,12 @@ package com.quickbite.order_service.service;
 
 import com.quickbite.core.security.UserRole;
 import com.quickbite.order_service.dto.*;
+import com.quickbite.order_service.dto.filter.OrderFilter;
 import com.quickbite.order_service.mappers.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -20,7 +23,7 @@ public class OrderService {
     private final OrderQueryService queryService;
     private final OrderStatusService statusService;
 
-    public List<OrderResponse> getUserOrders(Long userId) {
+    public List<OrderResponse> getUserOrders(Long userId)  {
         return queryService.getUserOrders(userId);
     }
 
@@ -28,8 +31,16 @@ public class OrderService {
         return queryService.getOrderById(id, userId, role);
     }
 
-    public List<OrderResponse> getRestaurantOrders(Long restaurantId) {
-        return queryService.getRestaurantOrders(restaurantId);
+    public Page<OrderResponse> getRestaurantOrders(Long restaurantId, Pageable pageable) {
+        return queryService.getRestaurantOrders(restaurantId, pageable);
+    }
+
+    public OrderStatusResponse getRestaurantStats(Long restaurantId) {
+        return queryService.getRestaurantStats(restaurantId);
+    }
+
+    public Page<OrderResponse> searchOrders(OrderFilter filter, Pageable pageable) {
+        return queryService.searchOrders(filter, pageable);
     }
 
     public OrderResponse createOrder(OrderRequest request, Long userId) {
