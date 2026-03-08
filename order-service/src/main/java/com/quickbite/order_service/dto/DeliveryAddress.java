1d35605f8af9c05afe0dc9ad1e41fc6b83cb22f4
icadorin <cadorinisrael@gmail.com>
2026-03-08 19:51:23 -0300

    feat(dto): add deliveryAddress DTO

---
+++ order-service/src/main/java/com/quickbite/order_service/dto/DeliveryAddress.java
@@ -0,0 +1,11 @@
+package com.quickbite.order_service.dto;
+
+public class DeliveryAddress {
+
+    private String street;
+    private String number;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String complement;
+}
