7905c0cb91713f75c6fc4414f145705a9fbd0524
icadorin <cadorinisrael@gmail.com>
2026-03-15 00:27:58 -0300

    feat(dto): add deliveryAddressRequest dto

---
+++ order-service/src/main/java/com/quickbite/order_service/dto/DeliveryAddressRequest.java
@@ -0,0 +1,32 @@
+package com.quickbite.order_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DeliveryAddressRequest {
+
+    @NotBlank
+    private String street;
+
+    @NotBlank
+    private String number;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String state;
+
+    @NotBlank
+    private String zipCode;
+
+    private String complement;
+}
+
